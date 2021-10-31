4496e43bad866dcf6d98e4cc3e6a620853bf75d3
Jomariette Kristine Suarez <jomariettekristine.suarez@gmail.com>
2021-10-31 20:23:14 +0800

    user table migration
    Created Users table migration

--- database/migrations/2021_10_31_105520_create_users_table.php
+++ database/migrations/2021_10_31_105520_create_users_table.php
@@ -15,12 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('email', 100)->nullable();
+            $table->string('username', 50)->unique();
+            $table->string('password', 100);
+            $table->string('first_name', 50);
+            $table->string('last_name', 50);
+            $table->char('gender', 1);
+            $table->string('registration_date')->useCurrent();
         });
     }
 
